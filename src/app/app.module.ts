1fc55988bb7fd71e6023f4511688ca9601501523
Davis Roldán <droldan-2019310@kinal.edu.gt>
2021-05-15 02:59:40 -0300

    Se agrego el home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FontAwesomeModule } from '@fortawesome/fontawesome-free';
+import { ImgFooterComponent } from './components/img-footer/img-footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NotFoundComponent,
+    CarouselComponent,
+    ImgFooterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
